f40d0b54d78fb1c82d3718fcfc1b7ff9eeaf6c61
Raphaël Henon <raph.henon@laposte.net>
2021-09-30 16:35:02 +0200

    update tp01 et ajout tp02

---
+++ JS/tp02.js
@@ -0,0 +1,5 @@
+let distance = prompt("Entrez la distance");
+let temps = prompt("Entrez le temps");
+let vitesse = distance / temps;
+let affichageResultat = `${vitesse} m/s`;
+console.log(affichageResultat);
