45762bdc52ec75bd962f6e96e92b330969efac31
Raphaël Henon <raph.henon@laposte.net>
2021-10-01 16:37:23 +0200

    ajout tp12+13

---
+++ JS/tp12.js
@@ -0,0 +1,6 @@
+let array = [15, 10, 20, 1, 12];
+let somme = 0;
+for (i = 0; i < array.length; i++) {
+  somme += array[i];
+}
+console.log(somme);
---
+++ JS/tp13.js
@@ -0,0 +1,7 @@
+let array = [1, 2, 3, 4, 5];
+let somme = 0;
+for (i = 0; i < array.length; i++) {
+  somme += array[i];
+}
+somme = somme / array.length;
+console.log(somme);
