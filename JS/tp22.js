c72ac4b30f2d2e51d4109360de9eaca93ba7af28
Raphaël Henon <raph.henon@laposte.net>
2021-10-04 17:42:39 +0200

    update tp22

--- JS/tp22.js
+++ JS/tp22.js
@@ -21,8 +21,8 @@ for (let i = 0; i < nbrEtudiant; i++) {
   const nom = prompt("Nom :");
   const prenom = prompt("Prénom :");
   const age = prompt("Age :");
-  const genre = toLowerCase(prompt("Genre :"));
-  const pays = toLowerCase(prompt("Pays :"));
+  const genre = prompt("Genre :");
+  const pays = prompt("Pays :");
   const option = prompt("Option :");
   const nbreCours = prompt("Nombre de cours :");
   const listeCours = [];
@@ -36,37 +36,53 @@ for (let i = 0; i < nbrEtudiant; i++) {
 console.log(listeEtudiant);
 for (let k = 0; k < nbrEtudiant; k++) {
   //Le nombre de personnes d'origine chinoise
-  if (listeEtudiant[k].pays === "chine") {
+  if (listeEtudiant[k].pays.toLowerCase() === "chine") {
     origineChinois += 1;
   }
   //La liste des personnes dont le prénom termine par "a"
-  if (listeEtudiant[k].prenom[listeEtudiant[k].prenom.length - 1] === "a") {
+  if (
+    listeEtudiant[k].prenom[
+      listeEtudiant[k].prenom.length - 1
+    ].toLowerCase() === "a"
+  ) {
     prenomEndA.push(listeEtudiant[k]);
   }
   //La liste de tout les noms
   listeNoms[k] = listeEtudiant[k].nom;
   //La liste de tout les hommes russes
-  if (listeEtudiant[k].pays === "russie" && listeEtudiant[k].genre === "m") {
+  if (
+    listeEtudiant[k].pays.toLowerCase() === "russie" &&
+    listeEtudiant[k].genre.toLowerCase() === "m"
+  ) {
     listeHommeRusse.push(listeEtudiant[k]);
   }
   //La liste de toutes les personnes dont les noms commencent par K et qui sont des femmes
-  if (listeEtudiant[k].prenom[0] === "k" && listeEtudiant[k].genre === "f") {
+  if (
+    listeEtudiant[k].prenom[0].toLowerCase() === "k" &&
+    listeEtudiant[k].genre.toLowerCase() === "f"
+  ) {
     listeFemmePrenomK.push(listeEtudiant[k]);
   }
   //La liste de tous les hommes dont les noms commencent par M
-  if (listeEtudiant[k].prenom[0] === "m" && listeEtudiant[k].genre === "m") {
+  if (
+    listeEtudiant[k].prenom[0].toLowerCase() === "m" &&
+    listeEtudiant[k].genre.toLowerCase() === "m"
+  ) {
     listeHommePrenomM.push(listeEtudiant[k]);
   }
 }
 console.log(`Nombre de personnes d'origine chinoise : ${origineChinois}`);
 console.log(
-  `Liste des personnes dont le prénom termine par "a" : ${prenomEndA}`
+  `Liste des personnes dont le prénom termine par "a" : `,
+  prenomEndA
 );
-console.log(`Liste de tout les noms : ${listeNoms}`);
-console.log(`Liste de tout les hommes russes : ${listeHommeRusse}`);
+console.log(`Liste de tout les noms : `, listeNoms);
+console.log(`Liste de tout les hommes russes : `, listeHommeRusse);
 console.log(
-  `Liste de toutes les personnes dont les noms commencent par K et qui sont des femmes : ${listeFemmePrenomK}`
+  `Liste de toutes les personnes dont les noms commencent par K et qui sont des femmes : `,
+  listeFemmePrenomK
 );
 console.log(
-  `Liste de toutes les personnes dont les noms commencent par M et qui sont des hommes : ${listeHommePrenomM}`
+  `Liste de toutes les personnes dont les noms commencent par M et qui sont des hommes : `,
+  listeHommePrenomM
 );
